feb938cd96490e0c1e6317a47427e6a344716a19
01283865399 <dyba8561@gmail.com>
2026-09-15 23:59:09 +0300

    Empty account page imports

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -1,32 +1,7 @@
-import { Hero } from '@/components/sections/hero';
-// @ts-ignore
-import { PrayerTimesSection } from '@/components/sections/prayer-times-section';
-// @ts-ignore
-import { QuranSection } from '@/components/sections/quran-section';
-// @ts-ignore
-import { DailyContent } from '@/components/sections/daily-content';
-// @ts-ignore
-import { HadithSection } from '@/components/sections/hadith-section';
-// @ts-ignore
-import { AzkarSection } from '@/components/sections/azkar-section';
-// @ts-ignore
-import { Features } from '@/components/sections/features';
-// @ts-ignore
-import { InteractiveFeatures } from '@/components/sections/interactive-features';
-
-export default function Home() {
+export default function AccountPage() {
   return (
-    <main className="min-h-screen relative">
-      <Hero />
-      <div className="relative z-10 flex flex-col gap-12 sm:gap-16 pb-20">
-        <PrayerTimesSection />
-        <DailyContent />
-        <QuranSection />
-        <HadithSection />
-        <AzkarSection />
-        <InteractiveFeatures />
-        <Features />
-      </div>
-    </main>
+    <div className="min-h-[60vh] flex items-center justify-center p-6 text-center">
+      <h1 className="text-2xl font-bold">صفحة الحساب قيد التجهيز</h1>
+    </div>
   );
 }
